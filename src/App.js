d0bea5cf1b1c8b8743a9cb9e603044448eca13bf
TobiasBanno <tobiasbanno@gmail.com>
2023-01-03 20:36:46 -0300

    subida

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import { Button } from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const[user, setUser]= useState(null);
+
+  //return<div>{user?<h1>Estas logeado</h1>:<h1>No estas logeado</h1>}</div>
+
+  return(
+    <div>
+      <Button variant="primary">Primary</Button>{' '}
+  </div>
+  )
+  
+  
+}
 export default App;
